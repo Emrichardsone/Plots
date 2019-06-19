a0a300221ae5cf528c700f6bb28f1838ad0b5da7
Emma <Emrichardsone@gmail.com>
2019-06-19 11:06:15 -0500

    Working with registration, user and password passed to view, login required

--- src/main/java/me/map/Plots/Controllers/HomeController.java
+++ src/main/java/me/map/Plots/Controllers/HomeController.java
@@ -1,9 +1,14 @@
 package me.map.Plots.Controllers;
 
+import me.map.Plots.Models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-    @Controller
+@Controller
+@RequestMapping(value="")
     public class HomeController {
 
         @RequestMapping(value = "")
@@ -11,5 +16,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
             return "index";
         }
 
-    }
+        @RequestMapping (value="registration", method= RequestMethod.GET)
+            public String registration(Model model){
+                model.addAttribute("user", new User());
+                return "registration";
+            }
+        @RequestMapping (value="registration", method=RequestMethod.POST)
+            public String showreg(@ModelAttribute User user){
+            return "testpost";
+            }
+
+        }
+
+
 
--- src/main/java/me/map/Plots/Models/User.java
+++ src/main/java/me/map/Plots/Models/User.java
@@ -1,5 +1,75 @@
 package me.map.Plots.Models;
 
+import org.hibernate.validator.constraints.Email;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+    //fields//
+    private Integer userId;
+
+    @NotNull
+    @Email
+    public String email;
+
+    @NotNull
+    public String password;
+
+    @NotNull
+    @Size(min=5, max=30)
+    private String username;
+
+
+    //Constructors://
+    public User (){
+
+    }
+    public User(String email, String password) {
+        this.userId = userId;
+        this.email = email;
+        this.password = password;
+    }
+
+
+        //Getters and Setters//
+
+     public Integer getUserId() {
+    return userId;
+    }
+
+    public void setUserid(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+
+    }
+
+
     //TODO Create UserFields //
-}
+
